aa9e67c22da55bcd374f0474405866eb4de540f4
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-04-28 11:32:53 -0300

    terminado

--- Assets/examen.cs
+++ Assets/examen.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 public class examen : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int cantidadDeUnidades;
+    public int periodoDeDias;
+    int kmRecorridos;
+    int litrosConsumidos;
+    double costoTotalCombustible;
+
     void Start()
     {
+      // verificar datos ingresados
+      if(periodoDeDias < 5){
+        Debug.Log("Por favor ingresar un periodo de dias mayor a 5");
+        return;
+      }
+      if(cantidadDeUnidades < 1){
+        Debug.Log("Por favor ingresar una cantida de unidades mayor a 1");
+        return;
+      }
+      
+      kmRecorridos = cantidadDeUnidades * 90;
+      kmRecorridos = kmRecorridos * periodoDeDias;
+      litrosConsumidos = kmRecorridos / 15;
+      if(kmRecorridos % 15 > 0){
+        litrosConsumidos = litrosConsumidos + 1;
+      }
+      costoTotalCombustible = litrosConsumidos * 130;
+
+      if (litrosConsumidos >= 100)
+      {
+         costoTotalCombustible = costoTotalCombustible * 0.8; 
+      }
+      Debug.Log("Una flota de "+ cantidadDeUnidades + " unidades trabajando durante " + periodoDeDias + " días implicará un gasto de " + costoTotalCombustible + " pesos en concepto de combustible");
         
     }
 
